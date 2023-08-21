c05262800c5e5b347d6fa4dda05956183fcf7642
samit-maersk <SamitKumar.Patel@maersk.com>
2023-08-21 17:04:16 +0200

    global error handling

--- src/main/java/com/example/webfluxhttpexchange/WebfluxHttpExchangeApplication.java
+++ src/main/java/com/example/webfluxhttpexchange/WebfluxHttpExchangeApplication.java
@@ -1,10 +1,15 @@
 package com.example.webfluxhttpexchange;
 
+import com.example.webfluxhttpexchange.utility.EmployeeNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class WebfluxHttpExchangeApplication {
@@ -18,6 +23,16 @@ public class WebfluxHttpExchangeApplication {
 		return WebClient
 				.builder()
 				.baseUrl(uri)
+				//Request Filter
+				.filter((request, next) -> next
+						.exchange(request)
+						.flatMap(response -> {
+							var httpStatus = response.statusCode();
+							if(httpStatus.is4xxClientError()) {
+								return Mono.error(new EmployeeNotFoundException(""));
+							}
+							return Mono.just(response);
+						}))
 				.build();
 	}
 
